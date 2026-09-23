8b8f88df8b7f76dfbe0e7dd239d45091fb3d0382
Shunichitube <tube5383@gmail.com>
2026-09-23 09:08:55 +0900

    Update js/ui.js for Stage 11

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,5 @@
 import { displayPlayer, STAT_LABELS } from './data.js';
-import { SPECIAL_ABILITY_DESCRIPTIONS } from './market.js?v=0.5.2';
+import { SPECIAL_ABILITY_DESCRIPTIONS } from './market.js?v=0.6.0';
 
 export const escapeHtml = value => String(value).replace(/[&<>"']/g, char => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;' }[char]));
 
