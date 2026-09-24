9dff9b64d30425cce700f95cc0c5b732394d0cf5
Shunichitube <tube5383@gmail.com>
2026-09-24 14:12:27 +0900

    Stage 21 publish modules js/league.js

--- js/league.js
+++ js/league.js
@@ -1,5 +1,5 @@
-import { createClub } from './data.js?v=0.16.1';
-import { simulateMatch } from './sim.js?v=0.16.1';
+import { createClub } from './data.js?v=0.16.2';
+import { simulateMatch } from './sim.js?v=0.16.2';
 import { createRandom } from './random.js';
 
 const CPU_CLUBS = [
