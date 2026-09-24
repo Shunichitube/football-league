1710b366af615194016d4f4adf1bfeeff2152d14
Shunichitube <tube5383@gmail.com>
2026-09-24 14:12:34 +0900

    Stage 21 publish modules js/storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,4 +1,4 @@
-import { ensurePlayerCompatibility } from './data.js?v=0.16.1';
+import { ensurePlayerCompatibility } from './data.js?v=0.16.2';
 import { createRandom } from './random.js';
 
 const KEY = 'football-league:slot:';
