8f13b1cda513de74b026c39f5c62271085b2dc11
Shunichitube <tube5383@gmail.com>
2026-09-24 14:10:19 +0900

    Stage 21 cache version js/rules.js

--- js/rules.js
+++ js/rules.js
@@ -1,4 +1,4 @@
-import { renewalFee } from './development.js?v=0.16.0';
+import { renewalFee } from './development.js?v=0.16.1';
 
 export const ACTION_TYPES = Object.freeze({
   DRAFT_PICK: 'DRAFT_PICK',
