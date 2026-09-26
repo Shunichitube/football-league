1c7e2cba8c501e36c102014ef67f37fc96782d71
Shunichitube <tube5383@gmail.com>
2026-09-26 12:09:44 +0900

    Align multiplayer module cache versions

--- js/multiplayer-draft.js
+++ js/multiplayer-draft.js
@@ -1,9 +1,9 @@
-import { decideCpuDraftAction } from './cpu.js?v=0.17.27';
+import { decideCpuDraftAction } from './cpu.js?v=0.17.30';
 import { displayPlayer } from './data.js?v=0.17.2';
-import { createAuctionPool, resolveDraftActions } from './market.js?v=0.17.2';
+import { createAuctionPool, resolveDraftActions } from './market.js?v=0.17.30';
 import { createRandom } from './random.js';
 import { ACTION_TYPES } from './rules.js?v=0.17.2';
-import { renderPlayerCard } from './ui.js?v=0.17.29';
+import { renderPlayerCard } from './ui.js?v=0.17.34';
 
 const SESSION_KEY = 'football-league:multiplayer-session';
 const SORT_KEY = 'football-league:multiplayer-draft-sort';
